4e590259825293b64f8f29c505fbf56cf2144821
PaulSaferio <owakipaul@gmail.com>
2022-09-28 11:46:50 +0000

    Updating, sorting, deleting, multiplication and scorering

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """Returns a key with the biggest integer value."""
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    orodha = list(a_dictionary.keys())[0]
+    kamu = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > kamu:
+            kamu = v
+            orodha = k
+    return (ret)
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    mpya = {}
+    for i in a_dictionary:
+        mpya[i] = a_dictionary[i] * 2
+    return mpya
